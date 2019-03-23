c8c5f7d6fd684adb2d05f63f690e2e75349d03f2
Albertocalib <albertocanal_1998@hotmail.es>
2019-03-23 15:45:05 +0100

    Cambios para hacer menu lateral desplegable, falta retocar

--- src/main/resources/static/js/Inicio.js
+++ src/main/resources/static/js/Inicio.js
@@ -1,10 +1,51 @@
 function cargarPagina(tipo) {
 
-    if (tipo === 1){
+    if (tipo === 1) {
         busquedaModelos();
-    }else if(tipo === 2){
+    } else if (tipo === 2) {
         busquedaEjemplares();
-    }else{
+    } else {
         busquedaProveedores();
     }
 }
+
+function cambiarExpandir() {
+    var el = document.getElementsByClassName("col-md-3");
+    for (var i = 0; i < el.length; i++) {
+        el[i].classList.add("col-md-4");
+    }
+    var el2 = document.getElementsByClassName("col-md-4");
+    for (var j = 0; j < el2.length; j++) {
+        el2[j].classList.remove("col-md-3");
+    }
+
+
+    $("#buttonCollapse").replaceWith("<a  id=\"buttonCollapse\" type=\"button\" data-target=\"#navLateral\" onclick=\"cambiarColapsar()\">\n" +
+        "                    <span class=\"navbar-toggler-icon\"></span>\n" +
+        "\n" +
+        "                </a>");
+    document.getElementById("navLateral").style.width = "250px";
+    document.getElementById("wrapper-full-page").style.marginLeft = "250px";
+    $("#navLateral").show();
+}
+
+function cambiarColapsar() {
+
+    var el = document.getElementsByClassName("col-md-4");
+    for (var i = 0; i < el.length; i++) {
+        el[i].classList.add("col-md-3");
+    }
+    var el2 = document.getElementsByClassName("col-md-3");
+    for (var j = 0; j < el2.length; j++) {
+        el2[j].classList.remove("col-md-4");
+    }
+
+    $("#buttonCollapse").replaceWith("<a  id=\"buttonCollapse\" type=\"button\" data-target=\"#navLateral\" onclick=\"cambiarExpandir()\">\n" +
+        "                    <span class=\"navbar-toggler-icon\"></span>\n" +
+        "\n" +
+        "                </a>");
+    $("#navLateral").hide();
+
+    document.getElementById("wrapper-full-page").style.marginLeft = "0px";
+}
+
